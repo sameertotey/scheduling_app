04f0d5efdf30a521eb8627ec75ed15c8f3bb5b83
Sameer Totey <sameertotey@hotmail.com>
2014-05-24 12:04:01 -0600

    complete omniauth specs

--- spec/support/omniauth.rb
+++ spec/support/omniauth.rb
@@ -8,6 +8,25 @@ def signin_with_omniauth(provider)
       "credentials" => {"token"=>"test token",
                         "secret"=>"test secret"}
   }
+  OmniAuth.config.mock_auth[:twitter] = {
+      "provider" => "#{provider}",
+      "uid" => "twt uid",
+      "info" => {"name"=>"test user"},
+      "credentials" => {"token"=>"test token",
+                        "secret"=>"test secret"}
+  }
+
+  OmniAuth.config.mock_auth[:twister] = :invalid_credentials
+
+  visit new_user_session_path
+  click_on "Sign in with #{provider.titleize}"
+end
+
+def signin_with_omniauth_fail(provider)
+  OmniAuth.config.test_mode = true
+
+  OmniAuth.config.mock_auth[:default] = :invalid_credentials
+
   visit new_user_session_path
   click_on "Sign in with #{provider.titleize}"
 end
