d0254cd5d54d129d38bf9d2f34b1a0c2aea19425
Sameer Totey <sameertotey@hotmail.com>
2014-06-21 18:09:14 -0600

    improved users admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,32 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])   
+    unless @user == current_user || current_user.admin?
+      redirect_to :back, :alert => "Access denied."
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes user_params
+      flash[:notice] = "#{@user.email} was successfully updated."
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy!
+    flash[:notice] = "#{@user.email} deleted."
+    redirect_to users_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:admin)
+  end
 end
