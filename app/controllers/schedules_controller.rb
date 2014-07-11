fda8fd47226512ee4290d8305935100e0b87523b
Sameer Totey <sameertotey@hotmail.com>
2014-07-11 15:58:36 -0600

    include sidekiq to submit a delayed schedule job

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -5,7 +5,7 @@ class SchedulesController < ApplicationController
   end
 
   def create
-    @events = Schedule.make_events(params[:schedule][:year].to_i, params[:schedule][:month].to_i)
+    @events = Schedule.delay.make_events(params[:schedule][:year].to_i, params[:schedule][:month].to_i)
     flash[:notice] = "Scheduling job was successfully submitted"
     redirect_to root_path
   end
