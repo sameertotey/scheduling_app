f88dd69dac53e1ca04fc11160712cd0625cb0f56
Sameer Totey <sameertotey@hotmail.com>
2014-08-20 15:31:51 -0600

    switch the javascript driver form selenium to webkit

--- spec/features/users/user_delete_spec.rb
+++ spec/features/users/user_delete_spec.rb
@@ -22,7 +22,7 @@ feature 'User delete', :devise, js: true do
     login_as(user, :scope => :user)
     visit edit_user_registration_path(user)
     click_button 'Cancel my account'
-    page.driver.browser.switch_to.alert.accept
+    page.driver.browser.accept_js_confirms
     expect(page).to have_content 'Bye! Your account was successfully cancelled. We hope to see you again soon.'
   end
 
